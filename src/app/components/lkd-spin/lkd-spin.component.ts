1867a2f202081dbf829cacbba892046ae413a754
Pablo Pimas <ku@likuid.eu>
2019-09-05 12:31:53 +0200

    formatter service color a gradient new functions

--- src/app/components/lkd-spin/lkd-spin.component.ts
+++ src/app/components/lkd-spin/lkd-spin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { FormatterService } from '../../services/lkd-formatter.service';
 
 @Component({
   selector: 'lkd-spin',
@@ -9,39 +10,46 @@ export class LkdSpinComponent implements OnInit {
 
   @Input() config: any;
 
-  @ViewChild('back', {static: true}) backGradient: ElementRef;
-  @ViewChild('image', {static: true}) frontImage: ElementRef;
-  @ViewChild('gradient', {static: true}) frontGradient: ElementRef;
-  @ViewChild('title', {static: true}) titleGradient: ElementRef;
-
-  public colorAT: string;
-  public colorBT: string;
-
-  public background: string;
-  public gradient: string;
-  public gradientTransparent: string;
+  @ViewChild('back', {static: true})      backGradient: ElementRef;
+  @ViewChild('image', {static: true})     frontImage: ElementRef;
+  @ViewChild('gradient', {static: true})  frontGradient: ElementRef;
+  @ViewChild('title', {static: true})     titleGradient: ElementRef;
 
   constructor(
-    public renderer: Renderer2
+    public renderer: Renderer2,
+    public formatter: FormatterService
   ) { }
 
   ngOnInit() {
-    this.colorAT = this.ttzeColor(this.config.colorA);
-    this.colorBT = this.ttzeColor(this.config.colorB);
-    this.gradient = `linear-gradient(to right bottom, ${this.config.colorA}, ${this.config.colorB})`;
-    this.gradientTransparent = `linear-gradient(to right bottom, ${this.colorAT}, ${this.colorBT})`;
-    this.background = `linear-gradient(to right bottom, ${this.config.colorA}, ${this.config.colorB}), ${this.config.image}`;
-    this.renderer.setStyle(this.backGradient.nativeElement, 'background', this.gradient);
-    this.renderer.setStyle(this.titleGradient.nativeElement, 'background', this.gradientTransparent);
-    this.renderer.setStyle(this.frontImage.nativeElement, 'background', this.background);
-    this.renderer.setStyle(this.frontImage.nativeElement, 'background-size', 'cover');
-    this.renderer.setStyle(this.frontImage.nativeElement, 'background-position', 'center');
-  }
+    // --- BACK SIDE BACKGROUND GRADIENT --------
 
-  ttzeColor(color: string) {
-    const colorT = color + 'bc';
-    console.log(colorT);
-    return colorT;
-  }
+    this.renderer.setStyle(
+      this.backGradient.nativeElement,
+      'background',
+      this.formatter.linearGradientBack(this.config.colorA, this.config.colorB)
+    );
 
+    // --- TITLE TRANSPARENT BACKGROUND GRADIENT
+
+    this.renderer.setStyle(
+      this.titleGradient.nativeElement,
+      'background',
+      this.formatter.linearGradientBack(
+        this.formatter.ttzeColorHEX(this.config.colorB),
+        this.formatter.ttzeColorHEX(this.config.colorB)
+      )
+    );
+
+    // --- HEADER LINEAR GRADIENT & IMAGE -------
+
+    this.renderer.setStyle(
+      this.frontImage.nativeElement,
+      'background',
+      this.formatter.linearGradientBack(
+        this.config.colorA,
+        this.config.colorB,
+        this.config.image
+      )
+    );
+  }
 }
--- src/app/services/lkd-formatter.service.ts
+++ src/app/services/lkd-formatter.service.ts
@@ -8,6 +8,22 @@ export class FormatterService {
 
   constructor() { }
 
+  // --- TRANSPARENTIZE COLORS -----------------------
+
+  ttzeColorHEX(color: string, ratio = 'bc') {
+    return color + ratio;
+  }
+
+  // --- LINEAR GRADIENT -----------------------------
+
+  linearGradientBack(colorA: string, colorB: string, image?: string) {
+    if (image) {
+      return `linear-gradient(to right bottom, ${colorA}, ${colorB}), ${image}`;
+    } else {
+      return `linear-gradient(to right bottom, ${colorA}, ${colorB})`;
+    }
+  }
+
   // --- CLASS GENERATOR -----------------------------
 
   generatorClassLKD(element: string, format: string[]): string {
